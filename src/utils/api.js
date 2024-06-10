2a276f0ebefaa3c07b2f041a67412c2de229a7b3
Haris Amjad <harisamjad56@gmail.com>
2024-06-10 08:55:32 +0500

    update commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = '99f7bbd3afd9c43f63515f0555218493';
+const apiKey = 'API_KEY';
 
 export const fetchCurrentWeather = (city, unit) => {
     return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`);
